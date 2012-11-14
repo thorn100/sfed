db5ec28b259ad82418a6d437d9e3f8d650feae35
Chris Bennett <thorn100@hotmail.com>
2012-11-13 20:02:07 -0600

    button styles

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
+    <head>
+        <!-- META DATA -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="MSSmartTagsPreventParsing" content="true" />
+        <meta http-equiv="imagetoolbar" content="no" />
+        <!--[if IE]><meta http-equiv="cleartype" content="on" /><![endif]-->
 
-    <?php include("includes/head.php"); ?>
+        <!-- SEO -->
+        <title>SFED Code Challenge</title>
+
+        <!-- ICONS -->
+        <link rel="shortcut icon" type="image/ico" href="assets/images/favicon.ico" />
+
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
+        <!--[if IE 9]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie8.css" /><![endif]-->
+        <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie7.css" /><![endif]-->
+
+        <!-- JQUERY -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
+
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="assets/scripts/global.js"></script>
+
+        <!-- STYLESHEET PRESENTATION -->
+        <style type="text/css">
+            .grid .grid-row { border: 1px solid #cccccc; }
+
+            .grid .grid-row { background: #FF4C4C; background: rgba(75,74,77,.7); }
+            .grid .grid-col:nth-of-type(1n) { background: #19EDED; background: rgba(242,242,242,.9); }
+            .grid .grid-col:nth-of-type(2n) { background: #4CC9C9; background: rgba(242,242,242,.5); }
+
+            .ribbon { margin-top: 10px; }
+
+            .btn { margin-bottom: 10px; }
+
+            .btn + .btn { margin-left: 20px; }
+        </style>
+    </head>
 
 <body>
     <?php include("includes/header.php"); ?>
@@ -21,10 +61,60 @@
                     <h3 class="hdg hdg-3">Heading 3 (16px/bold)</h3>
                     <h3 class="hdg hdg-3-alt">Heading 3 Alternate (14px/bold)</h3>
                     <h3 class="hdg hdg-3-alt2 hdg-caps">Heading 3 Alternate2 (12px/bold/caps)</h3>
+
+                    <p>Default Body Copy (12px/1.2) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui.</p>
                 </div>
 
                 <div class="ribbon txt-rev">
-                    <p>fjdsalkfjkdsajfkasdj</p>
+                    <h3 class="hdg-3-feat">Buttons</h3>
+                </div>
+
+                <a class="btn" href="#">Button (no icon)</a>
+                <a class="btn btn-icon" href="#">
+                    <i class="btn-icon-feed"></i>
+                    Feed Me
+                </a>
+                <a class="btn btn-icon" href="#">
+                    <i class="btn-icon-recipe"></i>
+                    My Recipie Box
+                </a>
+
+                <form class="form-newsletter">
+                    <input type="text" name="email-newsletter" id="email-newsletter" placeholder="Your Email Address" tabindex="2" />
+                    <button class="btn">Send It</button>
+                </form>
+
+                <div class="ribbon txt-rev">
+                    <h3 class="hdg-3-feat">Grid</h3>
+                </div>
+
+                <div class="grid">
+                    <div class="grid-row">
+                        <div class="grid-col grid-col-12">12 of 12</div>
+                    </div>
+
+                    <div class="grid-row">
+                        <div class="grid-col grid-col-3">3 of 12</div>
+                        <div class="grid-col grid-col-9 grid-col-last">9 of 12</div>
+                    </div>
+
+                    <div class="grid-row">
+                        <div class="grid-col grid-col-6">6 of 12</div>
+                        <div class="grid-col grid-col-6 grid-col-last">6 of 12</div>
+                    </div>
+
+                    <div class="grid-row">
+                        <div class="grid-col grid-col-9">9 of 12</div>
+                        <div class="grid-col grid-col-3 grid-col-last">3 of 12</div>
+                    </div>
+                </div>
+
+                <div class="ribbon txt-rev">
+                    <h3 class="hdg-3-feat">Blocks</h3>
+                </div>
+
+                <div class="ribbon txt-rev">
+                    <h3 class="hdg-3-feat">Media Elements</h3>
                 </div>
 
                 <div class="grid-row">
@@ -46,7 +136,6 @@
                 </div>
             </div>
         </div><!--/.content-->
-        <div class="push"></div>
     </div><!--/.wrapper-->
 
     <?php include("includes/footer.php"); ?>
