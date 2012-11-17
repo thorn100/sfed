8aa691b752362f853e0441116d4bbfaae714f990
Chris Bennett <thorn100@hotmail.com>
2012-11-17 17:00:53 -0600

    additional rgba color fallbacks, updating notes

---
+++ footer-test.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <head>
+        <!-- META DATA -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="MSSmartTagsPreventParsing" content="true" />
+        <meta http-equiv="imagetoolbar" content="no" />
+        <!--[if IE]><meta http-equiv="cleartype" content="on" /><![endif]-->
+
+        <!-- SEO -->
+        <title>SFED Code Challenge</title>
+
+        <!-- ICONS -->
+        <link rel="shortcut icon" type="image/ico" href="assets/images/favicon.ico" />
+
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
+        <!--[if IE 9]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie8.css" /><![endif]-->
+        <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie7.css" /><![endif]-->
+
+        <!-- JQUERY -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
+
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="assets/scripts/global.js"></script>
+
+        <!-- STYLESHEET PRESENTATION -->
+        <style type="text/css">
+            .grid .grid-row { border: 1px solid #cccccc; }
+
+            .grid .grid-row { background: #FF4C4C; background: rgba(75,74,77,.7); }
+            .grid .grid-col:nth-of-type(1n) { background: #19EDED; background: rgba(242,242,242,.9); }
+            .grid .grid-col:nth-of-type(2n) { background: #4CC9C9; background: rgba(242,242,242,.5); }
+
+            /*.ribbon { margin-top: 10px; }*/
+
+            /*.btn { margin-bottom: 10px; }*/
+
+            /*.btn + .btn { margin-left: 20px; }*/
+        </style>
+    </head>
+
+<body>
+    <?php include("includes/header.php"); ?>
+    
+    <div class="wrapper">
+        <div class="content section clearfix" role="main">
+            <div class="wrapper-inner clearfix">
+                Content
+            </div><!--/.wrapper-inner-->
+        </div><!--/.content-->
+    </div><!--/.wrapper-->
+
+    <?php include("includes/footer.php"); ?>
+</body>
+</html>
